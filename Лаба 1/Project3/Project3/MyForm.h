6a958f05304b82df84468f5710fa2a448a10018b
Roma <rymensol2402@gmail.com>
2021-09-13 02:31:03 +0300

    Исправил

--- Лаба 1/Project3/Project3/MyForm.h
+++ Лаба 1/Project3/Project3/MyForm.h
@@ -125,7 +125,7 @@ namespace Project3 {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(648, 391);
+			this->ClientSize = System::Drawing::Size(707, 391);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
