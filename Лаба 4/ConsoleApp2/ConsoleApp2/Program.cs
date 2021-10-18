96e891eaea6618ca89032ebd6fabfcb828d01ed1
Roma <rymensol2402@gmail.com>
2021-10-18 03:22:07 +0300

    17.10.2021

---
+++ Лаба 4/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+   public class Owner
+    {
+        public string name;
+        public string organization;
+        public uint id;
+    }
+    public class MYList<T> : List<T>
+    {
+        private Owner owner = new Owner { name = "Роман", organization = "BSTU", id = 1 };
+        private Date date = new Date { date = (DateTime.Now).ToString() };
+        public static MYList<T> operator +(MYList<T> a, T b)
+        {
+            MYList<T> result = new MYList<T>();
+            result = a;
+            result.Add(b);
+            return result;
+        }
+        public static MYList<T> operator --(MYList<T> a)
+        {
+            MYList<T> result = new MYList<T>();
+            result = a;
+            result.RemoveAt(result.Count() - 1);
+            return result;
+        }
+        public static bool operator !=(MYList<T> a, MYList<T> b)
+        {
+            if (a.Equals(b))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static bool operator ==(MYList<T> a, MYList<T> b)
+        {
+            if (a.Equals(b))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator true(MYList<T> a)
+        {
+            if (a == a.OrderBy(x => x))
+                return true;
+            else return false;
+        }
+        public static bool operator false(MYList<T> a)
+        {
+            return false;
+        }
+        class Date
+        {
+            public string date;
+        }
+       
+    }
+    public static class StaticOperation
+    {
+        public static int SUm(this MYList<int> a)
+        {
+            var sum = 0;
+            for (int i = 0; i < a.Count; i++)
+            {
+                sum += a[i];
+            }
+            return sum;
+        }
+        public static int Diff(this MYList<int> a)
+        {
+            var diff = 0;
+            diff = a.Max() - a.Min();
+            return diff;
+        }
+        public static int COunt(this MYList<int> a)
+        {
+            return a.Count;
+        }
+        public static int Words(this string a)
+        {
+            int u = 0;
+            for(int i=0;i<a.Length;i++)
+            {
+                if (a[i]==' ')
+                {
+                    u++;
+                }
+                if(i==a.Length-1)
+                {
+                    if(a[a.Length - 1]!=' ')
+                    {
+                        u++;
+                    }
+                   
+                }
+            }
+            return u;
+        }
+        public static void NULLof(this MYList<int> a)
+        {
+            for (int i = 0; i < a.Count; i++)
+            {
+                if(a[i]==null)
+                {
+                    Console.WriteLine("Ошибка:пустое значение");
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MYList<int> list1 = new MYList<int>() { 1, 2, 3, 45 };
+            MYList<int> list2 = new MYList<int>() { 1, 4, 3, 6 };
+
+            list2 = list2 + 9;
+            list1 = list1--;
+            foreach(var item in list2)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine("");
+            foreach (var item in list1)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine("");
+            bool res = list1 != list2;
+            Console.WriteLine(res);
+            Console.WriteLine("");
+
+            list1.SUm();
+            list2.Diff();
+            list2.COunt();
+
+            MYList<int> list3 = new MYList<int>();
+            list3.Add(3);
+            list3.Add(5);
+            list3.Add(6);
+            list3.Insert(3, 9);
+            string F = "DEL SEL PEL";
+            Console.WriteLine($"Количество слов:{ F.Words() }");
+            list3.NULLof();
+
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            foreach (var item in list1)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine(" ");
+            if (list2)
+            {
+                Console.WriteLine("Упорядочен");
+            }
+            else
+            {
+                Console.WriteLine("Не упорядочен");
+            }
+        }
+    }
+}
---
+++ Лаба 5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class Person:Adres,Show
+    {
+        public string name;
+        public string surname;
+        public virtual void show()
+        {
+            Console.WriteLine("Персона");
+        }
+        public override string ToString()
+        {
+            return "Персона:";
+        }
+        public virtual void input()
+        {
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите Фамилию");
+            surname= Console.ReadLine();
+            Console.WriteLine("Введите Адрес");
+            adres = Console.ReadLine();
+        }
+    }
+    interface Show
+    {
+        void show();
+        void input();
+    }
+    abstract class Adres
+    {
+        public string adres;
+    }
+    abstract class Client : Person
+    {
+        public override void  show()
+        {
+            base.show();
+            Console.WriteLine("Клиент:");
+        }
+        public override string ToString()
+        {
+            return "Клиент: " + name + " "+ surname+" " +adres  ;
+        }
+    }
+    abstract class shet:oper_shet
+    {
+        
+    }
+    abstract class oper_shet:Client
+    {
+        public string shett;
+        public override void input()
+        {
+            base.input();
+            Console.WriteLine("Введите id счета");
+            shett=Console.ReadLine();
+        }
+        public override void show()
+        {
+            Console.Write("ID Счета : ");
+            Console.WriteLine(shett);
+            base.show();
+            Console.WriteLine("Счет: ");
+        }
+        public override string ToString()
+        {
+            return base.ToString()+ " ID Счета : "+shett+" Счет: ";
+        }
+    }
+    sealed class val_shet:shet
+    {
+        public override void show()
+        {
+            base.show();
+            Console.WriteLine("Валютный");
+        }
+        public override string ToString()
+        {
+            return base.ToString()+"Валютный";
+        }
+    }
+    sealed class ras_shet:shet
+    {
+        public override void show()
+        {
+            base.show();
+            Console.WriteLine("Рассчетный");
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "Рассчетный";
+        }
+    }
+    sealed class nak_shet:shet
+    {
+        public override void show()
+        {
+            base.show();
+            Console.WriteLine("Накопительный");
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "Накопительный";
+        }
+    }
+    sealed class obs_shet:shet
+    {
+        public override void show()
+        {
+            base.show();
+            Console.WriteLine("Общий");
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "Общий";
+        }
+    }
+    sealed class card:Client
+    {
+        public string id;
+        public override void input()
+        {
+            base.input();
+            Console.WriteLine($"Введите id карты");
+            id=Console.ReadLine();
+        }
+        public override void show()
+        {
+            Console.WriteLine("Владелец карты:");
+            Console.WriteLine($"ID карты: {id}");
+            base.show();
+        }
+        public override string ToString()
+        {
+            return base.ToString()+ "ID карты: "+id;
+        }
+    }
+    class Printer
+    {
+        public virtual void IAmPrinting(object someObj)
+        {
+            Console.Write(someObj.GetType());
+        }
+    }
+    /// <summary>
+    interface interder
+    {
+        void sam();
+    }
+    abstract class der
+    {
+        public abstract void sam();
+    }
+    class cl1 : der, interder
+    {
+        private int num;
+        public override void sam()
+        {
+            num = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    class cl2 : der, interder
+    {
+        private bool isPressed { get; set; } = false;
+        public override void sam()
+        {
+            isPressed = Convert.ToBoolean(Console.ReadLine());
+        }
+    }
+
+    class cl3 : der, interder
+    {
+        private int count { get; set; } = 0;
+        public override void sam()
+        {
+            Console.WriteLine("Кликните на кнопку");
+            Console.ReadKey();
+            count++;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            obs_shet sh1 = new obs_shet();
+            sh1.input();
+            Console.WriteLine(sh1.ToString());
+            Printer k = new Printer();
+            k.IAmPrinting(sh1);
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            List<der> cl = new List<der>();
+            cl.Add(new cl1());
+            cl.Add(new cl2());
+            cl.Add(new cl3());
+            Printer printer = new Printer();
+            foreach (der item  in cl)
+            {
+                printer.IAmPrinting(item);
+                if (item is cl1)
+                    Console.WriteLine($"  cl1");
+                else if (item is cl2)
+                    Console.WriteLine($"  cl2");
+                else if (item is cl3)
+                    Console.WriteLine($"  cl3");
+                else
+                    Console.WriteLine("  nothing");
+            }
+        }
+    }
+}
