a8752342b5e91cdd4ac18815f6381f902c7021cb
Roma <rymensol2402@gmail.com>
2021-12-06 08:55:12 +0300

    06.12.2021

---
+++ Лаба 12/Lab12/Lab12/Program.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var train = Reflector.Create<Train>(null);
+            Console.WriteLine(train);
+            Reflector.AssemblyName(train);
+            Reflector.FieldsAndPropertiesInfo(train);
+            Reflector.Interfaces(train);
+            Reflector.MethodsByParameterType(train, typeof(bool));
+            Reflector.PublicContructors(train);
+            Reflector.PublicMethods(train);
+
+            var po = Reflector.Create<PO>(null);
+            Console.WriteLine(po);
+            Reflector.AssemblyName(po);
+            Reflector.FieldsAndPropertiesInfo(po);
+            Reflector.Interfaces(po);
+            Reflector.MethodsByParameterType(po, typeof(int));
+            Reflector.PublicContructors(po);
+            Reflector.PublicMethods(po);
+
+        }
+    }
+    public partial class Train
+    {
+        private string punktnaz;
+        public int id;
+        private int number;
+        private string timeotp;
+        private int kolkupe;
+        private int kolobj;
+        private int kolplaz;
+        private int kolluks;
+        public static int price;
+        public const int pass = 55;
+        private int exampleset;
+        public int Exampleset
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Как может быть отрицательным число?!!!!!");
+                }
+                else
+                {
+                    exampleset = value;
+                }
+            }
+        }
+        public int Count_suit;
+        public string Punktnaz
+        {
+            get
+            {
+                return punktnaz;
+            }
+            set
+            {
+                punktnaz = String.IsNullOrEmpty(value) ? "Нет значения" : value;
+            }
+        }
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Как может быть отрицательное номер поезда?!!!!!");
+                }
+                else
+                {
+                    number = value;
+                }
+            }
+        }
+        public string Timeotp
+        {
+            get
+            {
+                return timeotp;
+            }
+            set
+            {
+                timeotp = String.IsNullOrEmpty(value) ? "Нет значения" : value;
+            }
+        }
+        public int Kolkupe
+        {
+            get
+            {
+                return kolkupe;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Как может быть отрицательное число мест?!!!!!");
+                }
+                else
+                {
+                    kolkupe = value;
+                }
+            }
+        }
+        public int Kolobj
+        {
+            get
+            {
+                return kolobj;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Как может быть отрицательное число мест?!!!!!");
+                }
+                else
+                {
+                    kolobj = value;
+                }
+            }
+        }
+        public int Kolplaz
+        {
+            get
+            {
+                return kolplaz;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Как может быть отрицательное число мест?!!!!!");
+                }
+                else
+                {
+                    kolplaz = value;
+                }
+            }
+        }
+        public int Kolluks
+        {
+            get
+            {
+                return kolluks;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Как может быть отрицательное число мест?!!!!!");
+                }
+                else
+                {
+                    kolluks = value;
+                }
+            }
+        }
+        public void mainvivod()
+        {
+            Console.WriteLine($"Общее количество мест в вагоне: {kolkupe + kolobj + kolplaz + kolluks} \n" +
+                $"-------------------------------------------------------------------------------------------------------");
+            Console.WriteLine($"\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ \n");
+        }
+        public Train(int ids, string punkt, int num, string timeo, int kolk, int kolo, int kolp, int koll)
+        {
+            id = ids;
+            punktnaz = punkt;
+            number = num;
+            timeotp = timeo;
+            kolkupe = kolk;
+            kolobj = kolo;
+            kolplaz = kolp;
+            kolluks = koll;
+            count++;
+            Count_suit = kolkupe + kolobj + kolplaz + kolluks;
+        }
+        public Train()
+        {
+            punktnaz = "Минск";
+            number = 1;
+            timeotp = "13:00";
+            kolkupe = 15;
+            kolobj = 16;
+            kolplaz = 20;
+            kolluks = 13;
+            count++;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"Пункт назначения: {punktnaz} \n" +
+                $"ID: {id} \n" +
+                $"Номер поезда: {number} \n" +
+                $"Время отправления: {timeotp} \n" +
+                $"Количество мест купе: {kolkupe} \n" +
+                $"Количество общих мест: {kolobj} \n" +
+                $"Количество плацкартных мест: {kolplaz} \n" +
+                $"Количество люксовых мест: {kolluks} \n" +
+                $"-------------------------------------------------------------------------------------------------------");
+        }
+        static Train()
+        {
+            price = 45;
+            object tr = new Train(1);
+        }
+        private Train(object k)
+        {
+            Console.WriteLine($"Вызван закрытый конструктор\n-----------------------");
+        }
+        static public int count = 0;
+        static public void Count()
+        {
+            Console.WriteLine($"Количество объектов: {count}");
+        }
+    }
+
+    public class PO
+    {
+        public delegate void Operation(PO po);
+        public event Operation Upgrade;
+        public event Operation Work;
+        public void upgrade(PO po)
+        {
+            if (Upgrade != null)
+            {
+                Console.WriteLine("!!!Произошел Upgrade!!!");
+                Upgrade(po);
+            }
+            else
+            {
+                Console.WriteLine("Upgrade невозможен");
+            }
+
+        }
+        public void work(PO po)
+        {
+            if (Work != null)
+            {
+                Console.WriteLine("!!!Выполняется работа!!!");
+                Work(po);
+            }
+            else
+            {
+                Console.WriteLine("Работа не выполнена");
+            }
+        }
+
+    }
+}
---
+++ Лаба 12/Lab12/Lab12/Reflector.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace Lab12
+{
+    public static class Reflector
+    {
+        public static string Path = "D:\\ООП\\Лаба 12\\Lab12\\";
+        public static string PathName = "CheckList(lab12).txt";
+        public static void AssemblyName(object obj) //  класс, хранящий информацию о сборке
+        {
+            Type type = obj.GetType();
+            string Info = "Имя сборки: " + type.Name + '\n' + "--------------------------\n";
+            File.AppendAllText(Path + PathName, Info);
+        }
+         public static void PublicContructors(object obj)
+        {
+            string Info = "Есть ли публичные конструкторы: ";
+            Type type = obj.GetType();
+            ConstructorInfo[] ConstructorInfo = type.GetConstructors(); //возвращает все конструкторы данного типа в виде набора объектов ConstructorInfo
+            if (ConstructorInfo.Length > 0)
+                Info += "- да\n"+"-------------------------\n";
+            else
+                Info += " - нет\n" + "-------------------------\n";
+            File.AppendAllText(Path + PathName, Info);
+        }
+        public static List<string> PublicMethods(object obj)
+        {
+            string PublicMethodsString = "Публичные методы: \n";
+            Type type = obj.GetType();
+            List<string> PublicMethodsList = new List<string>();
+
+            var AllMethods = type.GetMethods(); // получает все методы типа в виде массива объектов MethodInfo
+            foreach (var Met in AllMethods)
+            {
+                PublicMethodsList.Add(Met.Name);
+                PublicMethodsString += Met.Name + "\n";
+            }
+
+            PublicMethodsString += "------------------------\n";
+            if (PublicMethodsList.Count == 0)
+                File.AppendAllText(Path + PathName, "Публичные методы: отсутствуют\n" +
+                    "--------------------------------\n");
+            else
+                File.AppendAllText(Path + PathName, PublicMethodsString);
+            return PublicMethodsList;
+        }
+        public static List<string> FieldsAndPropertiesInfo(object obj)
+        {
+            Type type = obj.GetType();
+            string Info = "Поля:\n";
+            List<string> FieldsAndProperties = new List<string>();
+            List<string> Propertiess = new List<string>();
+
+            var FieldsArr = type.GetFields(); // возвращает все поля данного типа в виде массива объектов FieldInfo
+
+            foreach (FieldInfo FI in FieldsArr)
+            {
+                FieldsAndProperties.Add(FI.Name);
+                Info += FI.Name + '\n';
+            }
+
+            if (FieldsAndProperties.Count==0)
+                Info = "Поля: отсутствуют\n";
+            Info += "\n";
+
+            var Properties = type.GetProperties(); // получает все свойства в виде массива объектов PropertyInfo
+
+            Info += "Свойства: \n";
+            foreach (var PI in Properties)
+            {
+                Propertiess.Add(PI.Name);
+                FieldsAndProperties.Add(PI.Name);
+                Info += PI.Name + '\n';
+            }
+
+            if (Propertiess.Count==0)
+                Info +="отсутствуют\n";
+
+            Info += "------------------------------------\n";
+            File.AppendAllText(Path + PathName, Info);
+            return FieldsAndProperties;
+        }
+        public static List<string> Interfaces(object obj)
+        {
+            Type type = obj.GetType();
+            string Info = "Реализованные интерфейсы:\n";
+            List<string> InterfacesList = new List<string>();
+
+            var Interfaces = type.GetInterfaces(); // получает все реализуемые данным типом интерфейсы в виде массива объектов Type
+
+            foreach (var I in Interfaces)
+            {
+                InterfacesList.Add(I.Name);
+                Info += I.Name + '\n';
+            }
+
+            Info += "\n";
+            if (InterfacesList.Count == 0)
+                File.AppendAllText(Path + PathName, "Реализованные интерфейсы: отсутствуют\n"+"-------------------------\n");
+            else
+                File.AppendAllText(Path + PathName, Info);
+            return InterfacesList;
+        }
+        public static void MethodsByParameterType(object obj, Type Parameter)
+        {
+            Type type = obj.GetType();
+            string Info = "Методы с типом параметра " + Parameter.Name + ":\n";
+            string InfoStart = Info + "\n";
+
+            var AllMethods = type.GetMethods(); //получает все методы типа в виде массива объектов MethodInfo
+            foreach (var Met in AllMethods)
+            {
+                var MIParameters = Met.GetParameters(); //Массив типа, содержащий сведения, соответствующие сигнатуре метода
+                foreach (var PI in MIParameters)
+                {
+                    if (PI.ParameterType == Parameter)
+                    {
+                        Info += Met.Name + '\n';
+                        break;
+                    }
+                }
+            }
+
+            Info += "\n";
+            if (InfoStart != Info)
+                File.AppendAllText(Path + PathName, Info);
+            else
+                File.AppendAllText(Path + PathName, "Методы с типом параметра " + Parameter.Name + ": отсутствут\n" + "-----------------------------------\n");
+        }
+        public static object? Invoke(object obj, string MethodName, object?[]? Parameters)
+        {
+            Type type = obj.GetType();
+            var Method = type.GetMethod(MethodName);
+            var Result = Method.Invoke(obj, Parameters);
+            return Result;
+        }
+        public static object? Create<T>(object?[]? Params)
+        {
+            Type type = typeof(T);
+            var Constructors = type.GetConstructors();
+            var Parameters = Constructors[0].GetParameters();
+            object? Result = null;
+
+            if (Params == null)
+            {
+                foreach (var Constructor in Constructors)
+                {
+                    if (Constructor.GetParameters().Length == 0)
+                    {
+                        Result = Constructor.Invoke(null);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                foreach (var Constructor in Constructors)
+                {
+                    if (Constructor.GetParameters().Length == Params.Length)
+                    {
+                        Result = Constructor.Invoke(Params);
+                        break;
+                    }
+                }
+            }
+            return Result;
+        }
+    }
+}
